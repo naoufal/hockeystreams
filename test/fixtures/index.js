92500600769ccedd8fc1a7520c3f8f28a0de1113
Naoufal Kadhom <naoufalkadhom@gmail.com>
2015-02-02 22:14:49 -0500

    add get live stream and get locations test

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -7,6 +7,15 @@ module.exports = {
     success: require('./get-live/success.json'),
     success_date: require('./get-live/success_date.json'),
     fail: require('./get-live/fail.json')
+  },
+  getLiveStream: {
+    success: require('./get-live-stream/success.json'),
+    success_location: require('./get-live-stream/success_location.json'),
+    fail: require('./get-live-stream/fail.json')
+  },
+  getLocations: {
+    success: require('./get-locations/success.json'),
+    fail: require('./get-live/fail.json')
   }
 };
 
